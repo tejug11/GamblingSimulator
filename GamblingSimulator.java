840d09b51edbebf7b57538279fdea7862044e8c4
Tejal Gaikwad <you@example.com>
2022-10-01 16:58:31 +0530

    Added : code for output for 20 days of playing everyday

--- GamblingSimulator.java
+++ GamblingSimulator.java
@@ -3,43 +3,47 @@ package com.bridelabz;
 import java.util.Scanner;
 
 public class GamblingSimulator {
-
-	static int amount_of_stake = 100;
-	static int bet_amount = 1;
+	static int amaunt_of_stake = 100;
+	static int bet_amaunt = 1;
 	static int won = 1;
 	static int lost = 0;
+	static int total_amaunt = 0;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Starting amount of stake is " + amount_of_stake);
-		System.out.println("Amount to bet after every play " + bet_amount);
-		int cashInHand = amount_of_stake;
+		System.out.println("Starting amount of stake is " + amaunt_of_stake);
+		System.out.println("Amount to bet after every play " + bet_amaunt);
+		for (int i = 1; i <= 20; i++) {
+			int cashInHand = amaunt_of_stake;
+			while (cashInHand > 50 && cashInHand < 150) {
+				int play = (int) Math.floor(Math.random() * 10) % 2;
+				System.out.println("Gamblers play:" + play);
 
-		int InHandcash = 0;
-		while (InHandcash > 50 && InHandcash < 150) {
-			int play = (int) Math.floor(Math.random() * 10) % 2;
-			System.out.println("Gamblers play:" + play);
+				switch (play) {
+				case 1:
+					cashInHand = cashInHand + bet_amaunt;
+					System.out.println("Total current cash gambler has " + cashInHand);
+					break;
+				case 0:
+					cashInHand = cashInHand - bet_amaunt;
+					System.out.println("Total current cash gambler has " + cashInHand);
+					break;
+				default:
+					System.out.println("default");
+				}
+				if (play == 1) {
+					System.out.println("Player won the current play");
+				} else {
+					System.out.println("Player lost the current play");
+				}
 
-			if (play == won) {
-				cashInHand = InHandcash + bet_amount;
-				System.out.println("Total current cash gambler has " + InHandcash);
-			} else if (play == lost) {
-				InHandcash = InHandcash - bet_amount;
-				System.out.println("Total current cash gambler has " + InHandcash);
-			} else {
-				System.out.println("default");
 			}
-			if (play == 1) {
-				System.out.println("Player won the current play");
-			} else {
-				System.out.println("Player lost the current play");
+			if (cashInHand == 150 || cashInHand == 50) {
+				System.out.println("Player would like to resign for current day" + i);
+
 			}
-
+			total_amaunt = total_amaunt + cashInHand;
 		}
-		if (InHandcash == 150 || InHandcash == 50) {
-			System.out.println("Player will resign for current day");
-
-		}
+		System.out.println("Total amount player has after 20 days as per day included = " + total_amaunt + " $");
 	}
-
 }
